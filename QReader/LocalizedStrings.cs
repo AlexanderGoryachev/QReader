b0bd12b91e1ebde3ff2cd67bf45ea2e67f2f3c8d
AlexanderGoryachev <Goryachev.Alexander.S@gmail.com>
2013-10-25 17:08:17 +0400

    Initial commit

---
+++ QReader/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using QReader.Resources;
+
+namespace QReader
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ QReader/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Windows.Storage;
+using System.IO.IsolatedStorage;
+using System.Collections.ObjectModel;
+
+namespace QReader
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        private IsolatedStorageSettings localSettings;
+        private ObservableCollection<History> settingsCollection = new ObservableCollection<History>();
+        public class History
+        {
+            public string Key {get; set;}
+            public string Value {get; set;}
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            if (localSettings != null) settingsCollection = (ObservableCollection<History>)localSettings.Values;
+            HistoryListBox.ItemsSource = settingsCollection;
+        }
+
+        private void QRScaner_ScanComplete(object sender, JeffWilcox.Controls.ScanCompleteEventArgs e)
+        {
+            MessageBox.Show(e.Result);
+            var counter = HistoryListBox.Items.Count;
+            settingsCollection.Add(new History { Key = counter.ToString(), Value = e.Result });
+            QRScaner.StartScanning();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            settingsCollection.Add(new History { Key = "1", Value = "qwerty" });
+        }
+
+        private void HistoryListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (HistoryListBox.SelectedIndex >= 0)
+            {
+                Clipboard.SetText(((History)HistoryListBox.SelectedItem).Value);
+                MessageBox.Show("Text copied to clipboard");
+            }
+        }
+    }
+}
